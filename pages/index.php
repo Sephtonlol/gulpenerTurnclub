3b08e24eb707b97d50d3bdaf7341b4d7256db1b6
Sagaana21 <Suthasahana@gmail.com>
2024-04-08 11:13:35 +0200

    header gemaakt

--- pages/index.php
+++ pages/index.php
@@ -46,8 +46,44 @@ if (isset($_POST['newBlog'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gulpener Turnclub</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
 </head>
 <body>
+
+<!--Logo-->
+<div id="Logo">
+<img src="images/turnclub-logo.png" alt="Logo">
+</div>
+
+<!-- Log in icon -->
+<div class="rectangle"></div>
+<h2 class="text-rectangle">Inloggen</h2>
+
+
+<!-- Begin pagina Text -->
+<div id="text">
+    <h1>Gulpener<br>Turnclub</h1>
+    <p>Landsraderweg 5, 6271 NT Gulpen</p>
+</div>
+
+<!-- Navigatie Balk-->
+<div id="navigatie-container">
+    <ul>
+        <li><a href=”index.php”>Homepage</a></li>
+        <li><a href=”#”>Vereniging</a></li>
+        <li><a href=”#”>Groepen</a></li>
+        <li><a href=”#”>Historie</a></li>
+        <li><a href=”#”>Foto's</a></li>
+        <li><a href=”#”>Nieuws</a></li>
+        <li><a href=”#”>Contact</a></li> 
+    </ul>  
+</div>
+
+<!-- ?? -->
     <form method="POST" action="index.php">
         <button type="submit" name="logoutsub">Log out</button> <br>
 
