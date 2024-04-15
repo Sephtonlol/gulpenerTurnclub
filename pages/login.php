af83eb07d9dd5bc1f3750c0b03cdc8d4b7bcf768
jessekuiper100 <145969453+jessekuiper100@users.noreply.github.com>
2024-04-15 17:00:31 +0200

    added header etc

--- pages/login.php
+++ pages/login.php
@@ -1,37 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Gulpener Turnclub</title>
-    <link rel="stylesheet" href="..\styling\login.css">
+    <link rel="stylesheet" href="..\styling\style.css" />
+    <link rel="stylesheet" href="..\styling\login.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+<header rel="stylesheet" href="..\C:\idunno\htdocs\gulpenerTurnclub\styling\style.css">
+<!--Logo-->
+<div id="Logo">
+<img src="../styling/images/turnclub-logo.png" alt="Logo">
+</div>
+
+<!-- Log in icon -->
+<div class="rectangle"></div>
+<h2 class="text-rectangle">Inloggen</h2>
+
+
+<!-- Begin pagina Text -->
+<div id="text">
+    <h1>Gulpener<br>Turnclub</h1>
+    <p>Landsraderweg 5, 6271 NT Gulpen</p>
+</div>
+
+<!-- Navigatie Balk-->
+<div id="navigatie-container">
+    <ul>
+        <li><a href=”index.php”>Homepage</a></li>
+        <li><a href=”#”>Vereniging</a></li>
+        <li><a href=”#”>Groepen</a></li>
+        <li><a href=”#”>Historie</a></li>
+        <li><a href=”#”>Foto's</a></li>
+        <li><a href=”#”>Nieuws</a></li>
+        <li><a href=”#”>Contact</a></li> 
+    </ul>  
+</div>
 </head>
-<body>
+<!
+  </head>
+  <body>
+    <div class= 'footer'> </div> 
     <div class="containerLogin">
+      <div class="card">
+        <div class="text.center">
+          <form method="POST">
+            <span for="">Name: </span><br />
+            <input type="text" name="getname" /><br />
 
-    <div class=card>
-        <div class=text.center>
-        <form method="POST">
-            
-        
-            <span for="">Name: </span><br>
-            <input type="text" name="getname"><br>
+            <span for="">Password: </span><br />
+            <input type="password" name="getpassword" /><br /><br />
 
-            <span for="">Password: </span><br>
-            <input type="password" name="getpassword"><br><br>
-
-          
             <button type="submit" name="logIn">Log in</button>
 
             <span>sign up instead</span>
-            <a href='signUp.php'>Sign Up.</a>
-         </form>
-</div>
-    </div>
+            <a href="signUp.php">Sign Up.</a>
+          </form>
+        </div>
+      </div>
 
-    <div class= card2> 
-        
-    </div> 
+      <div class="card2"></div>
 
          <?php
            if(isset($_POST['logIn'])){
@@ -59,14 +89,9 @@
             }
            }
          ?>
-
-</body>
-
-<div class='footer'>
-
-<div class='footer-text'>lorum lorum banaan, abacadabra </footer-text>
-<div-whitespace> </div-whitespace> 
-
- </footer>
- </div>
+      <div class="top">INLOGGEN  </div> 
+        <div-whitespace> </div-whitespace>
+      </div>
+    </div>
+  </body>
 </html>
