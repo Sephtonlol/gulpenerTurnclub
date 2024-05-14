8a24481a4f439eeee9ee01f2a26ee7386ade1873
jessekuiper100 <145969453+jessekuiper100@users.noreply.github.com>
2024-05-14 09:32:35 +0200

    foto's gallerij werken

---
+++ pages/gallery.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gulpener Turnclub</title>
+    <link rel="stylesheet" href="../styling/gallery.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crosso.com/css2?family=Archivo+Black&display=swap rel="stylesheet">
+</head>rigin>
+    <link href="https://fonts.googleapis">
+<body>
+    <div class= "page-name" >gallery</div>
+<div class="image_flex">
+
+    <div class="image-container">
+        <img src="../assets/images/galleryimages/IMG-20240417-WA0001.jpg">
+    </div>
+
+    <div class="image-container">
+        <img src="../assets/images/galleryimages/IMG-20240417-WA0002.jpg" alt="">
+    </div>
+
+    <div class="image-container">
+        <img src="../assets/images/galleryimages/429669251_908194747771447_1156375767718345662_n.jpg ">
+    </div>
+
+    <div class="image-container">
+        <img src="../assets/images/galleryimages/438222617_937470711510517_5206002319568830627_n.jpg">
+    </div>
+</div>
+</body>
+</html>
